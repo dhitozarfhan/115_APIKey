c1c60f74c2cbf1db7c26792cf3d5fd031e2f2759
dhitozarfhan <dhitozarfhan@gmail.com>
2025-11-12 11:56:10 +0700

    Konfigurasi koneksi MySQL

---
+++ apikeyC/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const crypto = require('crypto')
+const mysql = require('mysql2/promise')
+const app = express()
+const port = process.env.PORT || 3000
+
+// Konfigurasi koneksi MySQL
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',          // ubah sesuai user MySQL kamu
+  password: 'Superhero02',           // ubah kalau ada password
+  database: 'apikey'  // nama database kamu
+}
